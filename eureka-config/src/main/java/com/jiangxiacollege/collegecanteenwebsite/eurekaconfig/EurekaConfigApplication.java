624239fb8e40f4fb7701504bc9db09ec02456452
qianchuan <qianchuan@rxhpay.com>
2020-01-06 17:32:34 +0800

    1

---
+++ eureka-config/src/main/java/com/jiangxiacollege/collegecanteenwebsite/eurekaconfig/EurekaConfigApplication.java
@@ -0,0 +1,15 @@
+package com.jiangxiacollege.collegecanteenwebsite.eurekaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class EurekaConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigApplication.class, args);
+    }
+
+}
---
+++ eureka-server/src/test/java/com/jiangxiacollege/collegecanteenwebsite/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangxiacollege.collegecanteenwebsite.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
