6c11674ed3501da304481297fc273064ff73c8a1
qianchuan <qianchuan@rxhpay.com>
2020-01-07 11:46:47 +0800

    1

--- eureka-config/src/test/java/com/jiangxiacollege/collegecanteenwebsite/eurekaconfig/EurekaConfigApplicationTests.java
+++ eureka-config/src/test/java/com/jiangxiacollege/collegecanteenwebsite/eurekaconfig/EurekaConfigApplicationTests.java
@@ -1,13 +1,13 @@
-package com.jiangxiacollege.collegecanteenwebsite.eurekaconfig;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurekaConfigApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.jiangxiacollege.collegecanteenwebsite.eurekaconfig;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EurekaConfigApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
