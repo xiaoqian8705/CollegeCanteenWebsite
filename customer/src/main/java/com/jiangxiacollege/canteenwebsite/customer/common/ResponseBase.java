12ef1856c1373d3c8e52a6850ed4595a4f248b95
bysjwky <1045043267@qq.com>
2020-02-09 20:24:45 +0800

    订餐改

--- customer/src/main/java/com/jiangxiacollege/canteenwebsite/customer/common/ResponseBase.java
+++ customer/src/main/java/com/jiangxiacollege/canteenwebsite/customer/common/ResponseBase.java
@@ -1,12 +1,45 @@
 package com.jiangxiacollege.canteenwebsite.customer.common;
 
+
 import lombok.Data;
+
 import java.io.Serializable;
 
-@Data
 public class ResponseBase implements Serializable {
     private Integer code;
     private String message;
     private Object data;
     private PageInfo pageInfo;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public PageInfo getPageInfo() {
+        return pageInfo;
+    }
+
+    public void setPageInfo(PageInfo pageInfo) {
+        this.pageInfo = pageInfo;
+    }
 }
